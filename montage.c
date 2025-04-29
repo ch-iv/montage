8076b3376c08dd5445b1c44bfec596046166e288
ch-iv <icheppy101@gmail.com>
2025-04-29 14:07:18 -0400

    draw line numbers and smooth scrolling

--- montage.c
+++ montage.c
@@ -22,16 +22,33 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
 
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 #include "raylib.h"
+
+#define begin_timer()                                                          \
+  do {                                                                         \
+    _timer_start = clock();                                                    \
+  } while (0)
+
+#define end_timer()                                                            \
+  do {                                                                         \
+    clock_t _timer_end = clock();                                              \
+    double elapsed_time =                                                      \
+        ((double)(_timer_end - _timer_start)) / CLOCKS_PER_SEC;                \
+    printf("Elapsed time: %f seconds\n", elapsed_time);                        \
+  } while (0)
 
 #define SCREEN_WIDTH 1000
 #define SCREEN_HEIGHT 800
 #define MAX_LINE_LEN 200
 #define MAX_LINES 1024
+
+clock_t _timer_start;
 
 Color BACKGROUND_COLOR = (Color){26, 27, 38, 255};
 Color TYPE_COLOR = (Color){45, 188, 215, 255};
@@ -39,6 +56,7 @@ Color KEYWORD_COLOR = (Color){130, 95, 195, 255};
 Color STRING_COLOR = (Color){162, 220, 80, 255};
 Color DELIM_COLOR = (Color){170, 185, 255, 255};
 Color TEXT_COLOR = (Color){170, 185, 255, 255};
+Color TEXT_COLOR_DARKER = (Color){58, 65, 95, 255};
 
 float min_float(float a, float b) { return (a < b) ? a : b; }
 
@@ -57,12 +75,23 @@ char *keywords_list[] = {
 size_t types_count = sizeof(types_list) / sizeof(types_list[0]);
 size_t keywords_count = sizeof(keywords_list) / sizeof(keywords_list[0]);
 
+int alignRight(Rectangle child, Rectangle parent) {
+  return parent.width - child.width;
+}
+
 typedef struct Token {
   char *str;
   int size;
   Color color;
   bool delim;
 } Token;
+
+typedef struct Code {
+  char (*lines)[MAX_LINE_LEN];
+  Token (*tokens)[MAX_LINE_LEN];
+  int *ntokens;
+  int nlines;
+} Code;
 
 void colorize(Token *token) {
   if ((token->str)[0] == '"' || (token->str)[0] == '\'') {
@@ -141,76 +170,158 @@ void tokenize(char *str, Token *tokens, int *ntokens) {
   tokens[(*ntokens) - 1].str = copied_token;
 }
 
+void getCodeTexture(RenderTexture2D *target, int textureWidth,
+                    int textureHeight, Font font, int fontSize, int charSpacing,
+                    Code *code, bool drawLineNumbers) {
+  *target = LoadRenderTexture(textureWidth, textureHeight);
+  BeginTextureMode(*target);
+  int digitWidth = MeasureTextEx(font, "0", fontSize, charSpacing).x;
+  int maxLineDigits = (int)(log10(code->nlines)) + 1;
+  int lineNumOffset = digitWidth * maxLineDigits + 15;
+  char snum[maxLineDigits + 1];
+  int currTextX = drawLineNumbers ? lineNumOffset : 0;
+  int currTextY = 0;
+  for (int i = 0; i < code->nlines; i++) {
+    if (drawLineNumbers) {
+      int lineDigits = (int)(log10(i)) + 1;
+      int lineNumX =
+          alignRight((Rectangle){0, 0, lineDigits * digitWidth, 0},
+                     (Rectangle){0, 0, maxLineDigits * digitWidth, 0});
+      sprintf(snum, "%d", i);
+      DrawTextEx(font, snum, (Vector2){lineNumX, currTextY}, fontSize,
+                 charSpacing, TEXT_COLOR_DARKER);
+    }
+
+    for (int j = 0; j < code->ntokens[i]; j++) {
+      DrawTextEx(font, code->tokens[i][j].str, (Vector2){currTextX, currTextY},
+                 fontSize, charSpacing, code->tokens[i][j].color);
+      currTextX +=
+          MeasureTextEx(font, code->tokens[i][j].str, fontSize, charSpacing).x;
+    }
+
+    currTextY += 35;
+    currTextX = drawLineNumbers ? lineNumOffset : 0;
+  }
+  EndTextureMode();
+}
+
+Code parseCode(char *filename) {
+  FILE *f = fopen(filename, "r");
+  if (f == NULL) {
+    fprintf(stderr, "Can't open file %s\n", filename);
+    exit(EXIT_FAILURE);
+  }
+
+  Code code;
+  code.lines = calloc(MAX_LINES, sizeof(char[MAX_LINE_LEN]));
+  code.tokens = calloc(MAX_LINES, sizeof(Token[MAX_LINE_LEN]));
+  code.ntokens = calloc(MAX_LINES, sizeof(int));
+  code.nlines = 0;
+  for (int i = 0; i < MAX_LINES; i++) {
+    if (fgets(code.lines[i], MAX_LINE_LEN, f) == NULL) {
+      break;
+    }
+    code.lines[i][strlen(code.lines[i]) - 1] = '\0';
+    code.nlines++;
+  }
+
+  for (int i = 0; i < code.nlines; i++) {
+    tokenize(code.lines[i], code.tokens[i], &code.ntokens[i]);
+  }
+
+  for (int i = 0; i < code.nlines; i++) {
+    for (int j = 0; j < code.ntokens[i]; j++) {
+      colorize(&(code.tokens[i][j]));
+    }
+  }
+  return code;
+}
+
+double getSmoothScroll(double startTime, double startPos, double currTime,
+                       double timeToMove, double distToMove) {
+  double ratio = (currTime - startTime) / timeToMove;
+  if (ratio > 1) {
+    ratio = 1.0;
+  }
+  ratio = 1 - (1 - ratio) * (1 - ratio);
+  return startPos + ratio * distToMove;
+}
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     printf("Usage ./%s <filename.txt>\n", argv[0]);
     return 1;
   }
 
-  FILE *f = fopen(argv[1], "r");
-  if (f == NULL) {
-    fprintf(stderr, "Can't open file %s\n", argv[1]);
-    return 1;
-  }
-
-  char code[MAX_LINES][MAX_LINE_LEN];
-  int lines = 0;
-
-  for (int i = 0; i < MAX_LINES; i++) {
-    if (fgets(code[i], MAX_LINE_LEN, f) == NULL) {
-      break;
-    }
-    code[i][strlen(code[i]) - 1] = '\0';
-    lines++;
-  }
-
-  Token tokens[MAX_LINES][MAX_LINE_LEN] = {0};
-  int ntokens[MAX_LINES] = {0};
-
-  for (int i = 0; i < lines; i++) {
-    tokenize(code[i], tokens[i], &ntokens[i]);
-  }
-
-  for (int i = 0; i < lines; i++) {
-    for (int j = 0; j < ntokens[i]; j++) {
-      colorize(&tokens[i][j]);
-    }
-  }
+  Code code = parseCode(argv[1]);
 
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Montage");
   SetWindowState(FLAG_WINDOW_RESIZABLE);
-  SetTargetFPS(144);
+  MaximizeWindow();
+
   int fontSize = 30;
-  int codx = 50;
-  int cody = 10;
+  int lineHeight = fontSize + 5;
+  int charSpacing = 0;
 
   Font myfont = LoadFontEx("./resources/fonts/iosevka/Iosevka-Medium.ttf",
                            fontSize, NULL, 0);
-  SetTextureFilter(myfont.texture, TEXTURE_FILTER_POINT);
+
+  int textureWidth = GetScreenWidth();
+  int textureHeight = code.nlines * lineHeight;
+  RenderTexture2D target;
+  getCodeTexture(&target, textureWidth, textureHeight, myfont, fontSize,
+                 charSpacing, &code, true);
+  double codeY = 0;
+  double scrollStartTime = 0;
+  double scrollStartPos = codeY;
+  double scrollDirection = 0;
+  bool scrolling = false;
   while (!WindowShouldClose()) {
-    BeginDrawing();
-    ClearBackground(BACKGROUND_COLOR);
-    cody += 35 * GetMouseWheelMove();
-    int codcy = cody;
-    int codcx = codx;
-    int spacing = 0;
-    for (int i = 0; i < lines; i++) {
-      for (int j = 0; j < ntokens[i]; j++) {
-        DrawTextEx(myfont, tokens[i][j].str, (Vector2){codcx, codcy}, fontSize,
-                   spacing, tokens[i][j].color);
-        codcx += MeasureTextEx(myfont, tokens[i][j].str, fontSize, spacing).x;
-      }
-      codcy += 35;
-      codcx = codx;
+    if (textureWidth != GetScreenWidth()) {
+      textureWidth = GetScreenWidth();
+      getCodeTexture(&target, textureWidth, textureHeight, myfont, fontSize,
+                     charSpacing, &code, true);
     }
 
+    if (GetMouseWheelMove()) {
+      scrollStartTime = GetTime();
+      scrollStartPos = codeY;
+      scrollDirection = GetMouseWheelMove();
+      scrolling = true;
+    }
+
+    if (scrolling) {
+      double newY = getSmoothScroll(scrollStartTime, scrollStartPos, GetTime(),
+                                    0.15, 200 * scrollDirection);
+      if (newY == codeY) {
+        scrolling = false;
+      } else {
+        codeY = newY;
+      }
+    }
+    BeginDrawing();
+    ClearBackground(BACKGROUND_COLOR);
+    BeginBlendMode(3);
+    DrawTextureRec(
+        target.texture,
+        (Rectangle){0, 0, target.texture.width, -target.texture.height},
+        (Vector2){0, codeY}, RAYWHITE);
+    EndBlendMode();
+    DrawFPS(100, 100);
     EndDrawing();
   }
-  for (int i = 0; i < lines; i++) {
-    for (int j = 0; j < ntokens[i]; j++) {
-      free(tokens[i][j].str);
+
+  CloseWindow();
+
+  for (int i = 0; i < code.nlines; i++) {
+    for (int j = 0; j < code.ntokens[i]; j++) {
+      free(code.tokens[i][j].str);
     }
   }
-  CloseWindow();
+
+  free(code.lines);
+  free(code.tokens);
+  free(code.ntokens);
+
   return 0;
 }
